c1354b7e7d7fcb9a08512396179205b07ffc6285
franhermani <franhermani@gmail.com>
2020-04-26 16:01:42 -0300

    Fixed client and server main, no more loop

--- client.c
+++ client.c
@@ -1,16 +1,25 @@
 #include "common_socket.h"
 #include <stdio.h>
 
+#define RESPONSE_MAX_LEN    1024
+
 int main(int argc, char *argv[]) {
-    socket_t socket;
+    socket_t socket_client;
     const char *host = argv[1];
     const char *port = argv[2];
+    const char *request = argv[3];
+    char response[RESPONSE_MAX_LEN];
 
-    socket_create(&socket, host, port);
+    socket_create(&socket_client, host, port);
 
-    while (1) {
-        // socket_send(&socket, buffer, strlen(buffer));
-    }
+    socket_send(&socket_client, request, strlen(request));
+    printf("Sent request to server: %s\n", request);
+
+    socket_receive(&socket_client, response, strlen(response));
+    printf("Received response from server: %s\n", response);
+
+    printf("Shutting down socket...\n");
+    socket_close(&socket_client);
     return 0;
 }
 
--- server.c
+++ server.c
@@ -2,31 +2,29 @@
 #include <stdio.h>
 
 #define REQUEST_MAX_LEN    1024
-#define RESPONSE_MAX_LEN    1024
 
 int main(int argc, char *argv[]) {
     socket_t socket_acceptor, socket_client;
     const char *host = 0;
     const char *port = argv[1];
     char request[REQUEST_MAX_LEN];
-    char response[RESPONSE_MAX_LEN];
+    char *response = "OK";
 
     socket_create(&socket_acceptor, host, port);
     socket_listen(&socket_acceptor);
 
-    while (1) {
-        printf("Accepting connections...\n");
-        socket_accept(&socket_acceptor, &socket_client);
+    printf("Accepting connections...\n");
+    socket_accept(&socket_acceptor, &socket_client);
 
-        socket_receive(&socket_client, request, sizeof(request));
-        printf("Received from client: %s\n", request);
+    socket_receive(&socket_client, request, strlen(request));
+    printf("Received from client: %s\n", request);
 
-        socket_send(&socket_client, response, sizeof(response));
-        printf("Sent to client: %s\n", response);
+    socket_send(&socket_client, response, strlen(response));
+    printf("Sent to client: %s\n", response);
 
-        printf("Shutting down socket...\n");
-        socket_shutdown(&socket_client, SHUT_RDWR);
-        socket_close(&socket_client);
-    }
+    printf("Shutting down socket...\n");
+    socket_shutdown(&socket_client, SHUT_RDWR);
+    socket_close(&socket_client);
+    return 0;
 }
 
