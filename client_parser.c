971dd5b5dcfb3a53c6c9b97ba49d843640f0266c
franhermani <franhermani@gmail.com>
2020-04-28 22:53:20 -0300

    Dynamic buffer in parser (unused)

--- client_parser.c
+++ client_parser.c
@@ -5,8 +5,14 @@
 #define OK 0
 #define ERROR -1
 
-void parser_create(parser_t *self, const char *file_path) {
+int parser_create(parser_t *self, const char *file_path) {
+    dynamic_buffer_t *dyn_buf = dynamic_buffer_create();
+    if (! dyn_buf) return ERROR;
+
     self->file_path = file_path;
+    self->dyn_buf = dyn_buf;
+
+    return OK;
 }
 
 int parser_parse_input_file(parser_t *self) {
@@ -15,17 +21,32 @@ int parser_parse_input_file(parser_t *self) {
         printf("Error opening file\n");
         return ERROR;
     }
-    int i, s;
+
+    int i, s = 1;
+    bool eol;
+
     while (! feof(fd)) {
         i = 0;
-        while (i < sizeof(self->file_buffer)) {
-            s = fread(&self->file_buffer[i], sizeof(char), 1, fd);
-            if (self->file_buffer[i] == '\n' || s == 0) break;
+        eol = false;
+        while ((s != 0) && (i < sizeof(self->stat_buf))) {
+            s = fread(&self->stat_buf[i], sizeof(char), 1, fd);
+            if (self->stat_buf[i] == '\n') {
+                eol = true;
+                break;
+            }
             i += 1;
         }
-        self->file_buffer[i] = '\0';
-        memset(&self->file_buffer, 0, sizeof(self->file_buffer));
+        self->stat_buf[i] = '\0';
+        if (eol) {
+            // TODO: aplicar protocolo dbus a esta linea
+            //dynamic_buffer_remove_data(self->dyn_buf);
+        } else {
+            //dynamic_buffer_insert_data(self->dyn_buf, self->stat_buf);
+        }
+        printf("Buffer: %s\n", self->stat_buf);
+        memset(&self->stat_buf, 0, sizeof(self->stat_buf));
     }
+    //dynamic_buffer_destroy(self->dyn_buf);
     fclose(fd);
     return OK;
 }
--- client_parser.h
+++ client_parser.h
@@ -1,14 +1,17 @@
 #ifndef CLIENT_PARSER_H
 #define CLIENT_PARSER_H
 
+#include "client_dynamic_buffer.h"
+
 #define BUF_FILE_MAX_LEN 32
 
 typedef struct {
     const char *file_path;
-    char file_buffer[BUF_FILE_MAX_LEN];
+    char stat_buf[BUF_FILE_MAX_LEN];
+    dynamic_buffer_t *dyn_buf;
 } parser_t;
 
-void parser_create(parser_t *self, const char *file_path);
+int parser_create(parser_t *self, const char *file_path);
 
 int parser_parse_input_file(parser_t *self);
 
