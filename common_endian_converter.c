2bf7fdb74a7345fb3a1c1bfbc414c16f336d1ab1
franhermani <franhermani@gmail.com>
2020-05-03 16:40:24 -0300

    Endian converters working for numbers bigger than 1 byte

--- common_endian_converter.c
+++ common_endian_converter.c
@@ -2,11 +2,11 @@
 
 void big_to_little(uint32_t data, uint8_t *new_data) {
     new_data[0] = (data & 0x000000FF);
-    new_data[1] = (data & 0x000000FF) >> 8;
-    new_data[2] = (data & 0x000000FF) >> 16;
-    new_data[3] = (data & 0x000000FF) >> 24;
+    new_data[1] = (data & 0x0000FF00) >> 8;
+    new_data[2] = (data & 0x00FF0000) >> 16;
+    new_data[3] = (data & 0xFF000000) >> 24;
 }
 
 uint32_t little_to_big(uint8_t *data) {
-    return (data[3]) | (data[2]) | (data[1]) | (data[0]);
+    return (data[0]) | (data[1] << 8) | (data[2] << 16) | (data[3] << 24);
 }
