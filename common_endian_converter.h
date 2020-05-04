d380c03adba96bb3ce9fe859e2a30c0b226d152a
franhermani <franhermani@gmail.com>
2020-05-04 00:07:43 -0300

    Added documentation to endian converter methods

--- common_endian_converter.h
+++ common_endian_converter.h
@@ -3,8 +3,12 @@
 
 #include <stdint.h>
 
+// Converts the 'data' 4-byte number (in big endian)
+// to a 'new_data' 4-size array of bytes (in little endian)
 void big_to_little(uint32_t data, uint8_t *new_data);
 
+// Converts the 'data' 4-size array of bytes (in little endian)
+// to a 4-byte number (in big endian)
 uint32_t little_to_big(uint8_t *data);
 
 #endif // ENDIAN_CONVERTER_H
