da094cac99f3877b0f8ed40e7705e3f8fe5e97b3
franhermani <franhermani@gmail.com>
2020-05-13 17:07:07 -0300

    Convertor to big endian updated

--- common_endian_converter.c
+++ common_endian_converter.c
@@ -1,4 +1,5 @@
 #include "common_endian_converter.h"
+#include <byteswap.h>
 #include <stdbool.h>
 
 // Detects the current machine endianness
@@ -6,27 +7,17 @@
 static bool host_is_little();
 
 void host_to_little(uint32_t data, uint8_t *new_data) {
-    if (host_is_little()) {
-        new_data[0] = (data & 0x000000FF);
-        new_data[1] = (data & 0x0000FF00) >> 8;
-        new_data[2] = (data & 0x00FF0000) >> 16;
-        new_data[3] = (data & 0xFF000000) >> 24;
-    } else {
-        new_data[0] = (data & 0x000000FF);
-        new_data[1] = (data & 0x0000FF00);
-        new_data[2] = (data & 0x00FF0000);
-        new_data[3] = (data & 0xFF000000);
-    }
+    uint32_t data_swap = host_is_little() ? data : bswap_32(data);
+    new_data[0] = (data_swap & 0x000000FF);
+    new_data[1] = (data_swap & 0x0000FF00) >> 8;
+    new_data[2] = (data_swap & 0x00FF0000) >> 16;
+    new_data[3] = (data_swap & 0xFF000000) >> 24;
 }
 
 uint32_t little_to_host(uint8_t *data) {
-    uint32_t n;
-    if (host_is_little()) {
-        n = (data[0]) | (data[1] << 8) | (data[2] << 16) | (data[3] << 24);
-    } else {
-        n = (data[0]) | (data[1]) | (data[2]) | (data[3]);
-    }
-    return n;
+    uint32_t n = data[0] | (data[1] << 8) | (data[2] << 16) | (data[3] << 24);
+    uint32_t n_swap = host_is_little() ? n : bswap_32(n);
+    return n_swap;
 }
 
 static bool host_is_little() {
